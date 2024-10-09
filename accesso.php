4a6039f5972eb893f6bbf186d56a7090a8ae749c
l-savi <113691727+l-savi@users.noreply.github.com>
2024-10-09 16:28:24 +0200

    Create accesso.php

---
+++ accesso.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h2>Richiesta credenziali per entrare</h2>
+
+    <form action ="login.php" method="POST"></form>
+    <input type="text" name = "name">
+    <input type="password" name="password"><br>
+    <input type= "submit" value= "submit">                                                                                   
+
+</body>
+</html>
