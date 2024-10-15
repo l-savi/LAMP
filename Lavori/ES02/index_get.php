13616c4b49123d9f03560f32f855fd821bafb00d
l-savi <113691727+l-savi@users.noreply.github.com>
2024-10-15 21:10:44 +0200

    Create index_get.php

---
+++ Lavori/ES02/index_get.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accesso a pagina riservata</title>
+</head>
+<body>
+    <h1>Accesso a pagina riservata</h1>
+    <p>Accesso consentito solo agli utenti registrati</p>
+
+    <form action="accesso.php" method="GET">
+
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username">
+        <br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+        <br>
+        <input type="submit" value="Accedi">
+        <input type="reset" value="Annulla">
+
+    </form>
+</body>
+</html>
