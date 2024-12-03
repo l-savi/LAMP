58fa0ad9b5491663b04d5718fa25eb8cd237ed1e
l-savi <leoninoleonardo1990@gmail.com>
2024-12-03 12:38:01 +0000

    a

--- mysql/biblioteca_v10_231123.sql
+++ mysql/biblioteca_v10_231123.sql
@@ -14,6 +14,115 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autori`
+--
+
+DROP TABLE IF EXISTS `autori`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autori` (
+  `id_autore` int NOT NULL AUTO_INCREMENT,
+  `nome_autore` varchar(50) NOT NULL,
+  `nazionalita` varchar(50) DEFAULT NULL,
+  `data_nascita` date DEFAULT NULL,
+  `data_morte` date DEFAULT NULL,
+  `nazionalita_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_autore`),
+  KEY `nazionalita_id` (`nazionalita_id`),
+  CONSTRAINT `autori_ibfk_1` FOREIGN KEY (`nazionalita_id`) REFERENCES `nazionalita` (`id_nazionalita`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autori`
+--
+
+LOCK TABLES `autori` WRITE;
+/*!40000 ALTER TABLE `autori` DISABLE KEYS */;
+INSERT INTO `autori` VALUES (1,'Jane Austen',NULL,'1775-12-16','1817-07-18',3),(2,'George Orwell',NULL,'1903-06-25','1950-01-21',3),(3,'Harper Lee',NULL,'1926-04-28','2016-02-19',4);
+/*!40000 ALTER TABLE `autori` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorie` (
+  `id_categoria` int NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorie`
+--
+
+LOCK TABLES `categorie` WRITE;
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+INSERT INTO `categorie` VALUES (1,'Romanzo'),(2,'Saggio'),(3,'Fantascienza'),(4,'Mistero');
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libri`
+--
+
+DROP TABLE IF EXISTS `libri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libri` (
+  `id_libro` int NOT NULL AUTO_INCREMENT,
+  `titolo` varchar(100) NOT NULL,
+  `id_autore` int DEFAULT NULL,
+  `id_categoria` int DEFAULT NULL,
+  `anno_pubblicazione` int DEFAULT NULL,
+  PRIMARY KEY (`id_libro`),
+  KEY `id_autore` (`id_autore`),
+  KEY `id_categoria` (`id_categoria`),
+  CONSTRAINT `libri_ibfk_1` FOREIGN KEY (`id_autore`) REFERENCES `autori` (`id_autore`),
+  CONSTRAINT `libri_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorie` (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libri`
+--
+
+LOCK TABLES `libri` WRITE;
+/*!40000 ALTER TABLE `libri` DISABLE KEYS */;
+INSERT INTO `libri` VALUES (1,'Orgoglio e pregiudizio',1,1,1813),(2,'1984',2,3,1949),(3,'Il buio oltre la siepe',3,1,1960),(4,'Guerra e pace',1,1,1869);
+/*!40000 ALTER TABLE `libri` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nazionalita`
+--
+
+DROP TABLE IF EXISTS `nazionalita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nazionalita` (
+  `id_nazionalita` int NOT NULL AUTO_INCREMENT,
+  `nome_nazionalita` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_nazionalita`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nazionalita`
+--
+
+LOCK TABLES `nazionalita` WRITE;
+/*!40000 ALTER TABLE `nazionalita` DISABLE KEYS */;
+INSERT INTO `nazionalita` VALUES (1,'Italiano'),(2,'Spagnolo'),(3,'Inglese'),(4,'Americano'),(5,'Cinese');
+/*!40000 ALTER TABLE `nazionalita` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +133,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-26 20:08:58
+-- Dump completed on 2024-12-03 12:14:42
