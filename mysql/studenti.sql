f0fbf7a708d66df6a8b055e1deb26a9474e00eb6
l-savi <leoninoleonardo1990@gmail.com>
2025-01-23 07:55:10 +0000

    a

--- mysql/studenti.sql
+++ mysql/studenti.sql
@@ -2,108 +2,109 @@ CREATE DATABASE IF NOT EXISTS studenti;
 USE studenti;
 
 
-CREATE TABLE corsi (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS corso_studi (
+    codice INT AUTO_INCREMENT PRIMARY KEY,
+    nome_corso VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE studenti (
+
+INSERT INTO corso_studi(codice, nome_corso)
+VALUES
+    ('liceo aristico'),
+    ('liceo scientifico'),
+    ('liceo linguistico');
+
+SELECT * FROM corso_studi;
+
+
+CREATE TABLE IF NOT EXISTS disciplina (
+    codice_disciplina INT AUTO_INCREMENT PRIMARY KEY,
+    nome_disciplina VARCHAR(30) NOT NULL
+);
+
+
+INSERT INTO disciplina(codice, nome_disciplina)
+VALUES
+    ('arte'),
+    ('matematica'),
+    ('italiano'),
+    ('inglese');
+
+SELECT * FROM disciplina;
+
+
+CREATE TABLE IF NOT EXISTS studenti (
     matricola INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cognome VARCHAR(100) NOT NULL,
-    data_nascita DATE NOT NULL,
-    FK_corsi INT NOT NULL,
-    FOREIGN KEY (FK_corsi) REFERENCES corsi(id) ON DELETE NO ACTION ON UPDATE CASCADE
-);
-
-CREATE TABLE materie (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE valutazioni (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    FK_studenti INT NOT NULL,
-    FK_materie INT NOT NULL,
-    voto INT NOT NULL,
-    FOREIGN KEY (FK_studenti) REFERENCES studenti(matricola) ON DELETE NO ACTION ON UPDATE CASCADE,
-    FOREIGN KEY (FK_materie) REFERENCES materie(id) ON DELETE NO ACTION ON UPDATE CASCADE
+    cognome VARCHAR(30) NOT NULL,
+    data_nascita DATE,
+    FK_corso FOREIGN KEY
+    capo_g INT
+    FOREIGN KEY(FK_corso) REFERENCES corso_studi(codice)
 );
 
 
-INSERT INTO corsi (nome) VALUES
-('Informatica'),
-('Matematica'),
-('Scienze'),
-('Lingue'),
-('Geografia'),
-('Economia');
+
+INSERT INTO studenti(matricola, cognome, data_nascita, capo_g)
+VALUES
+    ('rossi',15-03-2004,03),
+    ('menicucci',09-02-2007,02),
+    ('santoro',26-02-2007,01);
+
+SELECT * FROM studenti;
 
 
-INSERT INTO studenti (nome, cognome, data_nascita, FK_corsi) VALUES
-('Mario', 'Rossi', '1995-05-20', 1),
-('Giuseppe', 'Bianchi', '1998-08-15', 2),
-('Luigi', 'Verdi', '1997-03-10', 3),
-('Anna', 'Neri', '1996-12-12', 4),
-('Giorgio', 'Gialli', '1999-07-05', 1),
-('Antonio', 'Pellegrini', '1998-11-01', 2),
-('Emanuele', 'Russo', '1997-06-07', 3),
-('Francesco', 'Marconi', '1996-10-03', 4);
+
+CREATE TABLE IF NOT EXISTS valutazioni (
+    codice_val INT AUTO_INCREMENT PRIMARY KEY,
+    voto INT,
+    data_voto DATE,
+    FK_studenti FOREIGN KEY
+    FK_disc INT
+    FOREIGN KEY(FK_studenti) REFERENCES studenti(matricola)
+    FOREIGN KEY(FK_disc) REFERENCES disciplina(codice_disciplina)
+);
 
 
-INSERT INTO materie (nome) VALUES
-('Algebra'),
-('Geometria'),
-('Fisica'),
-('Italiano'),
-('Inglese'),
-('Lettere'),
-('Matematica');
+
+INSERT INTO valutazioni(matricola, voto, data_voto)
+VALUES
+    ( 8 , 15-03-2004),
+    ( 9, 09-02-2007),
+    ( 7,26-02-2007);
+
+SELECT * FROM valutazioni;
 
 
-INSERT INTO valutazioni (FK_studenti, FK_materie, voto) VALUES
-(1, 1, 8),
-(1, 2, 9),
-(1, 3, 10),
-(1, 4, 7),
-(1, 5, 8),
-(2, 1, 9),
-(2, 2, 8),
-(2, 3, 9),
-(2, 4, 9),
-(2, 5, 9),
-(3, 1, 8),
-(3, 2, 7),
-(3, 3, 8),
-(3, 4, 9),
-(3, 5, 8),
-(4, 1, 9),
-(4, 2, 9),
-(4, 3, 8),
-(4, 4, 9),
-(4, 5, 9),
-(5, 1, 9),
-(5, 2, 9),
-(5, 3, 9),
-(5, 4, 9),
-(5, 5, 9),
-(6, 1, 8),
-(6, 2, 9),
-(6, 3, 8),
-(6, 4, 9),
-(6, 5, 8),
-(7, 1, 9),
-(7, 2, 9),
-(7, 3, 9),
-(8, 2, 9),
-(8, 3, 9),
-(8, 4, 9),
-(8, 5, 9);
-
-SELECT s.cognome, v.voto
+SELECT s.cognome, v.voto 
 FROM studenti s, valutazioni v
-WHERE s.matricol = v.FK_studenti;
+WHERE s.matricola = v.FK_studenti;
 
-SELECT max (voto), min (voto), avg (voto);
-FROM 
-WHERE
+SELECT max(voto), min(voto), avg(voto);
+FROM valutazioni v, studenti s
+WHERE s.matricola = v.FK_studenti
+AND s.cognome = 'santoro'
+
+SELECT count(*) FROM studenti
+
+SELECT count(voto) FROM valutazioni
+
+
+SELECT s.cognome, v.disciplina, v.voto, YEAR(CURDATE()) - YEAR(s.data_nascita) AS eta --La funzione curdate è utilizzata per ottenere la data corrente nel formato YYYY-MM-DD. 
+FROM  Studenti s
+JOIN  Voti v ON s.matricola = v.matricola
+WHERE s.cognome LIKE 'S%';  
+
+
+SELECT s.cognome, COUNT(v.voto) AS numero_voti, AVG(v.voto) AS media_voti, MAX(v.voto) AS voto_piu_alto, MIN(v.voto) AS voto_piu_basso
+FROM Studenti s
+JOIN Voti v ON s.matricola = v.matricola
+WHERE s.matricola = '1' 
+
+SELECT COUNT(*) AS studenti_over_18
+FROM Studenti
+WHERE YEAR(CURDATE()) - YEAR(data_nascita) > 18;
+
+SELECT s.cognome
+FROM Studenti s
+LEFT JOIN Voti v ON s.matricola = v.matricola
+WHERE v.matricola IS NULL;
