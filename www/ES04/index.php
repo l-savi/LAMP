9ec7277269bcc7b6533f710ff18c6fcce8cee8f9
l-savi <113691727+l-savi@users.noreply.github.com>
2025-03-10 19:02:40 +0100

    Create index.php

---
+++ www/ES04/index.php
@@ -0,0 +1,15 @@
+<!--Pagina di benvenuto che mostri un messaggio di benvenuto e un link per accedere alla pagina di login.-->
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Benvenuto</title>
+    </head>
+    <body>
+        <h1>Benvenuto in questa pagina!</h1>
+        <p>Questa è una pagina di benvenuto, per accedere alla pagina di login clicca 
+            <a href="login.php">qui</a>.</p>
+    </body>
+</html>
