6dedc9cec59f50cecc239d5cf1768ff88f4de740
l-savi <113691727+l-savi@users.noreply.github.com>
2025-04-09 22:03:01 +0200

    Create riservata.php

---
+++ www/ES05/c/riservata.php
@@ -0,0 +1,33 @@
+<?php
+    require 'functions.php';
+
+    session_start();
+
+    [$sessionRetval, $sessionRetmsg] = checkSession();
+
+    if ($sessionRetval == false) {
+        $url = 'Location: login.php';
+        $url .= '?from='.basename($_SERVER['PHP_SELF']);
+        $url .= '&error=' . $sessionRetmsg;
+        header($url);
+        die();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Pagina Riservata</title>
+</head>
+<body>
+    <?="<h3>Benvenuto " . $_SESSION['username'] . "</h3>"?>
+
+    <a href="index.php">Home page</a>
+    <br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
