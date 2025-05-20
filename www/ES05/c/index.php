1406d7e3760d5ee48aa150f685070171dece092b
l-savi <leoninoleonardo1990@gmail.com>
2025-05-20 15:56:04 +0000

    a

--- www/ES05/c/index.php
+++ www/ES05/c/index.php
@@ -17,7 +17,7 @@ $html_link2 .= '<a href="riservata.php">Pagina Riservata</a>';
 </head>
 <body>
     <h2>Accedi</h2>
-    <form method="POST" action="">
+    <form method="POST" action="login.php">
         <label>Username:</label><br>
         <input type="text" name="username" required><br><br>
 
