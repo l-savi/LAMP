6f9efd8783b60b291e2d8a31683ca1d364de05fd
l-savi <leoninoleonardo1990@gmail.com>
2025-05-20 16:09:49 +0000

    a

--- www/ES05/c/login.php
+++ www/ES05/c/login.php
@@ -13,37 +13,40 @@ if(isset($_SESSION['username'])) {
 else if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-  try{
-    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-    if (!$conn) {
-        die("Connessione fallita: " . mysqli_connect_error());
-    }
-
-    // Query per selezionare tutti i record dalla tabella users
-    $query = "SELECT UserID FROM utente where Username = '$username' and Password = '$password';";
+    {
+        // Connessione al database
+        $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     
-
-    // Esecuzione della query
-    $result = mysqli_query($conn, $query);
-
-    if ($result) {
-        // Controllo se ci sono record        
-        if (mysqli_num_rows($result) > 0) {    //controllo se equivale a uno, funzione che fa ritornare num di quante righe controllate 
-            return [true, 'Login avvenuto con successo'];
-        } else {
-            return [false, 'Login sbagliato'];
+        if (!$conn) {
+            die("Connessione fallita: " . mysqli_connect_error());
         }
-
-        // Reset dei risultati
-        mysqli_free_result($result);                  
-    } else {
-        return [false, 'Errore: ' . mysqli_error($conn)];
-    }
-
-    // Chiusura della connessione
-    mysqli_close($conn);
-  }
+    
+        // Query per selezionare tutti i record dalla tabella users
+        $query = "SELECT UserID FROM utente where Username = '$username' and Password = '$password';";
+        
+    
+        // Esecuzione della query
+        $result = mysqli_query($conn, $query);
+    
+        if ($result) {
+            // Controllo se ci sono record        
+            if (mysqli_num_rows($result) > 0) {    //controllo se equivale a uno, funzione che fa ritornare num di quante righe controllate 
+                return [true, 'Login avvenuto con successo'];
+            } else {
+                return [false, 'Login sbagliato'];
+            }
+    
+            // Reset dei risultati
+            mysqli_free_result($result);                  
+        } else {
+            return [false, 'Errore: ' . mysqli_error($conn)];
+        }
+    
+        // Chiusura della connessione
+        mysqli_close($conn);
+      }
+    
+}
 ?>
 
 <?php
