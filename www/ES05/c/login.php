912b2cd90c46ff216b80c350f7a9670b4bc374dc
l-savi <leoninoleonardo1990@gmail.com>
2025-05-21 11:23:25 +0000

    a

--- www/ES05/c/login.php
+++ www/ES05/c/login.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (mysqli_num_rows($risultato) > 0) {
         $_SESSION['username'] = $username;
-        header("Location: riservata.php");
+        header("Location: test.php");
     } else {
         echo "Nome utente o password errati.";
     }
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<!-- Form HTML -->
+
 <!DOCTYPE html>
 <html>
 <head>
